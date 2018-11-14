74df6c0fa044c0c6741a3aa12607d407cdc791a6
H.c <liangyonghui123@qq.com>
2018-11-14 15:05:05 +0800

    add py

---
+++ sum.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+a = 8
+b = 4
+num1 = input("first number: ")
+num2 = input("last number: ")
+print "%s + %s = %s" % (num1,num2,num1+num2)
+print "%s - %s = %s" % (num1,num2,num1-num2)
+print "%s * %s = %s" % (num1,num2,num1*num2)
+print "%s / %s = %s" % (num1,num2,num1/num2)
